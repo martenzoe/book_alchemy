8d189a1857339d11ad30b7f054087d00187bec01
martenzoe <biz.zoellner@gmail.com>
2025-02-13 16:32:27 +0100

    Setting the Database URI and initialized Flask

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from data_models import db, Author, Book
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
