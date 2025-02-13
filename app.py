078d02251f433f06c2cebbc4a4c50230eace1015
martenzoe <biz.zoellner@gmail.com>
2025-02-13 19:00:04 +0100

    Changed the URI to the absolute path

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ from data_models import db, Author, Book
 app = Flask(__name__)
 
 # Configure the SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/martenzollner/Desktop/new_projects/book_alchemy/data/library.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoid unnecessary warnings
 
 # Bind the database to the app
 db.init_app(app)
 
 # Create the tables (run once)
-with app.app_context():
-    db.create_all()
+'''with app.app_context():
+    db.create_all()'''
 
 
 if __name__ == '__main__':
